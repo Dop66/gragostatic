20ea889f95772f545630bb46bc23e6ffb65d81fa
doppe <73268853+Dop66@users.noreply.github.com>
2026-03-18 02:43:31 +0000

    Pegando icones de jogadores da api e colocando no card

--- pages/rank/ranks/ranking.js
+++ pages/rank/ranks/ranking.js
@@ -15,6 +15,9 @@ async function carregarRank() {
             const resposta = await fetch(infoPlayers[t]);
             const dadosCompletos = await resposta.json();
 
+            const id = dadosCompletos.iconeId;
+            const urlImage = "https://ddragon.leagueoflegends.com/cdn/14.5.1/img/profileicon/" + id + ".png";
+
             const elos = dadosCompletos.elos;
             const soloQ = elos.find(fila => fila.queueType === "RANKED_SOLO_5x5");
             const flex = elos.find(fila => fila.queueType === "RANKED_FLEX_SR");
@@ -23,12 +26,12 @@ async function carregarRank() {
             // console.log("Jogador encontrado:", nomeReal);
             if(soloQ.leaguePoints != undefined){
                 container.innerHTML += `
-                        <div class="bg-white p-6 rounded-lg w-full border border-blue-500 shadow-md flex items-center justify-between gap-4">
+                        <div class="bg-[#000000]/80 backdrop-blur-md p-6 rounded-lg w-full border border-blue-500 shadow-md flex items-center justify-between gap-4">
                             <div class="flex items-center gap-4">
-                                <img src="https://opgg-static.akamaized.net/meta/images/profile_icons/profileIcon2075.jpg?image=q_auto:good,f_webp,w_200&v=1773443739" alt="Logo do Jogador" class="w-14 h-14 rounded-full border-2 border-gray-200 object-cover">
+                                <img src="${urlImage}" alt="Logo do Jogador" class="w-14 h-14 rounded-full border-2 border-gray-200 object-cover">
                                 
                                 <div class="flex flex-col justify-center">
-                                    // <p class="text-[#0d0d0d] font-bold text-xl leading-none mb-1">#2</p>
+                                    <!-- <p class="text-[#0d0d0d] font-bold text-xl leading-none mb-1">#2</p> --!>
                                     <p class="font-bold text-[#BD3C00] text-lg leading-none">${nomeReal}</p>
                                 </div>
                                 
@@ -36,16 +39,16 @@ async function carregarRank() {
                             
                             <div class="flex flex-row items-center gap-8">
                                 
-                                <div class="flex flex-col items-center text-sky-950">
-                                    <p class="text-sm font-semibold mb-1">SOLO/DUO</p>
+                                <div class="flex flex-col items-center text-sky-950 text-white">
+                                    <p class=" font-semibold mb-1">SOLO/DUO</p>
                                     <img src="assets/elos_assets/5.png" alt="rank" class="w-[80px]">
-                                    <p class="font-bold mt-1">${soloQ.leaguePoints}</p>
+                                    <p class="font-bold mt-1">${soloQ.leaguePoints} PDL</p>
                                 </div>
                                 
-                                <div class="flex flex-col items-center text-sky-950">
+                                <div class="flex flex-col items-center text-sky-950 text-white">
                                     <p class="text-sm font-semibold mb-1">FLEX</p>
                                     <img src="assets/elos_assets/5.png" alt="rank" class="w-[80px]">
-                                    <p class="font-bold mt-1">${flex.leaguePoints}</p>
+                                    <p class="font-bold mt-1">${flex.leaguePoints} PDL</p>
                                 </div>
 
                             </div>
